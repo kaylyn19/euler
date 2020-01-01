4fe401a488ae1a1bd05f181bf926f73fb042ae93
Kaylyn Kwak <kkwak0803@gmail.com>
2019-12-31 17:48:50 -0800

    largest_palindrome_product

--- largest_palindrome_product.py
+++ largest_palindrome_product.py
@@ -5,11 +5,11 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
 def largest_palindrome():
-  for i in range(999, 100, -1):
-    for j in range(999, 100, -1):
+  for i in range(999, 900, -1):
+    for j in range(999, 900, -1):
       product = i * j
       if str(product) == str(product)[::-1]:
         print ('i ==> ', i, 'j ==>', j)
-        return ('product', product)
+        return 'product', product
 
 print (largest_palindrome())
